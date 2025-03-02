2e030a0564beb9f494ea3c062edd961c9f379dd6
ChernyshevZahar <chernyshevzahar@gmail.com>
2025-03-02 23:13:09 +0300

    Добавил str функцию в модель LinkBase

--- RDRCBotLink/redirector/models.py
+++ RDRCBotLink/redirector/models.py
@@ -5,6 +5,10 @@ class LinkBase(models.Model):
     getlink = models.CharField(max_length=100)
     postLink =  models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.name
+
+
 
 class utmBase(models.Model):
     unicKye = models.CharField(max_length=100)
