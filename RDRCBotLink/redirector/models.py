1ee7dd85159235673a35fe6d372798068273c4c3
ChernyshevZahar <chernyshevzahar@gmail.com>
2025-03-02 21:23:12 +0300

    Создал класс LinkBase

--- RDRCBotLink/redirector/models.py
+++ RDRCBotLink/redirector/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class LinkBase(models.Model):
+    name = models.CharField(max_length=100)
+    getlink = models.CharField(max_length=100)
+    postLink =  models.CharField(max_length=100)
