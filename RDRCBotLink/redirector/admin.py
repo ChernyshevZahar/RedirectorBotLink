31d669b527b385dc2b32ba201f5e3dcb26fb1b91
ChernyshevZahar <chernyshevzahar@gmail.com>
2025-03-02 21:38:41 +0300

    Добавил utmBase в админку

--- RDRCBotLink/redirector/admin.py
+++ RDRCBotLink/redirector/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 
-from .models import LinkBase
+from .models import LinkBase , utmBase
 @admin.register(LinkBase)
 class LinkBase(admin.ModelAdmin):
     list_display  = ['name',]
+
+@admin.register(LinkBase)
+class utmBase(admin.ModelAdmin):
+    list_display  = ['unicKye','utms','linkbase',]
 
 
